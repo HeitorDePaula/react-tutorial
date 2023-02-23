662540d1fa5e15be9b57d198090e8cf2d78ce8f2
Heitor de paula <heitoraugusto989@gmail.com>
2023-02-23 18:00:14 -0300

    react tutorial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import MainMenu from './components/MainMenu'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MainMenu />
+    </>
+  )
+}
+
+export default App
